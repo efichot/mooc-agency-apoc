387190d460018cbc99b56531e10fbdd13a4a3dc4
arnaudambro <arnaud.ambroselli@yahoo.fr>
2018-06-15 08:29:16 +0200

    rectified bug with intro buttons

--- src/js/views/ChapterPresentation.js
+++ src/js/views/ChapterPresentation.js
@@ -6,45 +6,51 @@ import Iframe from 'react-iframe';
 import ButtonPrimary from './UI/ButtonPrimary';
 import Fade from '../transitions/Fade';
 
-class ChapterPresentation extends React.Component {
-  state = {};
-
-  render() {
-    const { classProps, name, description, button, videoUrl } = this.props;
-
-    return (
-      <Fade
-        in={this.props.in}
-        classProps={`${classProps}__chapter-presentation chapter-presentation`}
-      >
-        <div className="left-column">
-          <span className="title">{name}</span>
-          <p className="description">{description}</p>
-          {button && (
-            <Link to={button.link}>
-              <ButtonPrimary name={button.name} classProps={button.classes} />
-            </Link>
-          )}
-        </div>
-        <div className="right-column video-wrapper">
-          <Iframe
-            title={name}
-            url={`${videoUrl}`}
-            className="video"
-            position="relative"
-            display="block"
-            frameborder="0"
-            allowFullScreen
-            style={{
-              width: '100%',
-              height: 'auto'
-            }}
-          />
-        </div>
-      </Fade>
-    );
-  }
-}
+const ChapterPresentation = ({
+  in: inProps,
+  classProps,
+  name,
+  description,
+  button,
+  videoUrl
+}) => {
+  console.log('name, button', name, button);
+  return (
+    <Fade
+      in={inProps}
+      classProps={`${classProps}__chapter-presentation chapter-presentation`}
+    >
+      <div className="left-column">
+        <span className="title">{name}</span>
+        <p className="description">{description}</p>
+        {button && (
+          <Link to={button.link}>
+            <ButtonPrimary
+              name={button.name}
+              classProps={button.classes}
+              enableClick
+            />
+          </Link>
+        )}
+      </div>
+      <div className="right-column video-wrapper">
+        <Iframe
+          title={name}
+          url={`${videoUrl}`}
+          className="video"
+          position="relative"
+          display="block"
+          frameborder="0"
+          allowFullScreen
+          style={{
+            width: '100%',
+            height: 'auto'
+          }}
+        />
+      </div>
+    </Fade>
+  );
+};
 
 ChapterPresentation.propTypes = {
   in: PropTypes.bool,
