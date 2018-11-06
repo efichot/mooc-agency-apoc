71a025e570f4b14f764599c1ea5973749cfe8585
root <root@vps607582.ovh.net>
2018-11-06 10:14:03 +0100

    fist commit after linux vps deployment

--- src/js/model/static/step1/module_01_blocVideo.js
+++ src/js/model/static/step1/module_01_blocVideo.js
@@ -11,10 +11,10 @@ const module_01_blocVideo = {
   secondDescription: {
     __html: `Un fonds de placement, dit aussi fonds d’investissement, est un véhicule qui permet à des épargnants de détenir collectivement des actifs financiers et en particulier&nbsp;: 
 
-- des actions (titres de propriété représentant une fraction de capital d’une entreprise et donnant droit à un dividende qui varie en fonction des résultats de l’entreprise et qui peut être nul), 
-- des obligations (titres représentant une fraction de dette à plus d’un an d’une entreprise, d’un État ou organisme public, et assortis de la double obligation de verser une rémunération et d’être remboursés à leur échéance sauf cas de faillite), 
-- des titres monétaires (titres de même nature que les obligations mais à moins d’un an). 
-En regroupant les capitaux, le fonds de placement assure le partage des coûts mais aussi des avantages qui découlent du placement de sommes plus importantes, comme par exemple la possibilité de diversifier les placements et donc de mieux répartir les risques. 
+- des actions (titres de propriété représentant une fraction de capital d’une entreprise et donnant droit à un dividende qui varie en fonction des résultats de l’entreprise et qui peut être nul),
+- des obligations (titres représentant une fraction de dette à plus d’un an d’une entreprise, d’un État ou organisme public, et assortis de la double obligation de verser une rémunération et d’être remboursés à leur échéance sauf cas de faillite),
+- des titres monétaires (titres de même nature que les obligations mais à moins d’un an).
+En regroupant les capitaux, le fonds de placement assure le partage des coûts mais aussi des avantages qui découlent du placement de sommes plus importantes, comme par exemple la possibilité de diversifier les placements et donc de mieux répartir les risques.
 
 Il existe différents types de fonds en fonction du profil et des besoins des épargnants. `,
   },
