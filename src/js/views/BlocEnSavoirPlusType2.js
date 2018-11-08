fdde1d1822c5885d8852561f7136b39fee9f4593
sysadmintma <sysadmin@themoocagency.com>
2018-11-08 13:05:09 +0100

    5421 problème de popup qui faisait bouger l'exercice

--- src/js/views/BlocEnSavoirPlusType2.js
+++ src/js/views/BlocEnSavoirPlusType2.js
@@ -100,7 +100,9 @@ class BlocEnSavoirPlusType2 extends React.Component {
         {!noChapter && <BlocHeader type="horloge" duration={duration} name={chapter} />}
         <span className="bloc__name">{title}</span>
         <BlocDescription modulType={modulType} classProps="bloc__first-description" description={firstDescription} />
-        <div className="bloc-en-savoir-plus-type-2__cards game">
+        <div className="bloc-en-savoir-plus-type-2__cards game" style={{ height: 326 + 'px' }}>
+          {' '}
+          //modifDaivis
           <div className="bloc-en-savoir-plus-type-2__cards--to-hover">
             {cards.map((card, index) => {
               return (
@@ -138,6 +140,7 @@ class BlocEnSavoirPlusType2 extends React.Component {
               <span className="bloc-en-savoir-plus-type-2__victory-message">{victoryMessage}</span>
             </PopupBlue>
           )}
+
           <ButtonPrimary
             minWidth
             name="valider"
