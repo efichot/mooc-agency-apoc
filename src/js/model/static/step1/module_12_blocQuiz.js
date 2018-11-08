65c44b21a1e42bcebfd2e5d1b31aa994854aec5b
sysadmintma <sysadmin@themoocagency.com>
2018-11-08 15:50:30 +0100

    5421 Modification du quiz final

--- src/js/model/static/step1/module_12_blocQuiz.js
+++ src/js/model/static/step1/module_12_blocQuiz.js
@@ -17,7 +17,7 @@ const module_12_blocQuiz = {
     {
       questionNumber: 1,
       question: {
-        __html: `1 - Quelles questions doit examiner l’épargnant avant d’investir&nbsp;?`,
+        __html: `1 - Quelles questions doit examiner l’épargnant avant de placer son argent&nbsp;?`,
       },
       answers: [
         {
@@ -34,7 +34,7 @@ const module_12_blocQuiz = {
         },
       ],
       explication: {
-        __html: `La réponse à ces questions va déterminer son profil et l’allocation d’actifs qui lui correspond c’est-à-dire la proportion d’actifs risqués (actions) et moins risqués (obligations et titres monétaires) qui devraient composer son investissement. Plus son horizon est éloigné, plus sa capacité à prendre du risque est forte ; n’ayant pas besoin de liquidités à brève échéance, il n’est pas contraint de sécuriser ses placements pour éviter d’avoir à les vendre au plus mauvais moment, en cas de baisse brutale des marchés.`,
+        __html: `La réponse à ces deux questions va déterminer son profil et l’allocation d’actifs qui lui correspond c’est-à-dire la proportion d’actifs risqués (actions) et moins risqués (obligations et titres monétaires) qui devraient composer son placement. Plus son horizon est éloigné, plus sa capacité à prendre du risque est forte ; n’ayant pas besoin de liquidités à brève échéance, il n’est pas obligé de sécuriser ses placements pour éviter d’avoir à les vendre au plus mauvais moment, en cas de baisse brutale des marchés financiers.`,
       },
     },
     {
@@ -80,76 +80,76 @@ const module_12_blocQuiz = {
         },
       ],
       explication: {
-        __html: `Le détenteur d'obligations détient une partie de la dette des États et entreprises qui les ont émises. Il reçoit donc à ce titre une rémunération et il est remboursé à leur échéance, sauf cas de faillite. Pour les actions, il n'y a en revanche aucune obligation de rémunération (le dividende peut être nul), ni de remboursement (le détenteur d'actions a le statut de propriétaire), ce qui explique que leur risque, et donc la volatilité de leurs performances, soient plus grands.`,
+        __html: `Détenir des obligations revient à détenir une partie de la dette des États, organismes publics et entreprises concernés. Le fonds qui détient des obligations reçoit donc à ce titre une rémunération et il est remboursé à leur échéance, sauf cas de faillite de l’État, de l’organisme public ou de l’entreprise. Pour les actions, il n'y a en revanche aucune obligation de rémunération (le dividende peut être nul), ni de remboursement (le détenteur d'actions a le statut de propriétaire), ce qui explique que leur risque, et donc la volatilité de leurs performances, soient plus grands.`,
       },
     },
     {
       questionNumber: 4,
       question: {
-        __html: `4 - À quoi sert l'indice de référence d’un fonds&nbsp;?`,
+        __html: `4 - Quelle caractéristique rend les fonds monétaires peu risqués&nbsp;?`,
       },
       answers: [
         {
-          content: 'A - Calculer sa performance',
+          content: 'A - Leur performance est toujours positive',
           correctAnswer: false,
         },
         {
-          content: `B - Mesurer la capacité du gérant à ajouter de la performance`,
+          content: `B - La durée des titres détenus est courte`,
           correctAnswer: true,
         },
         {
-          content: 'C - Connaître les performances futures du fonds',
+          content: 'C - Leur rendement est garanti',
           correctAnswer: false,
         },
       ],
       explication: {
-        __html: `Comme son nom l'indique, l'indice de référence permet de voir si la performance de fonds est supérieure, égale ou inférieure à celle des marchés sur lesquels il est investi. Le fonds pouvant détenir des titres différents de ceux de l'indice, ce dernier ne permet ni de calculer les performances, ni de les prévoir avec certitude.`,
+        __html: `La durée courte des titres détenus, et donc leur remboursement à brève échéance, donnent beaucoup de visibilité sur la rémunération attendue. Mais cette rémunération est liée au niveau des taux d’intérêt ; elle peut être négative et n’est pas garantie. L’Autorité des Marchés Financiers (AMF) définit les fonds monétaires en fonction de la durée des titres. Elle distingue les fonds monétaires court terme des fonds monétaires dont les titres peuvent avoir une durée un peu plus longue et qui peuvent donc être plus rémunérateurs.`,
       },
     },
     {
       questionNumber: 5,
       question: {
-        __html: `5 - En fonction de quel critère l’AMF classe-t-elle les fonds monétaires&nbsp;?`,
+        __html: `5 - De quoi est composé un fonds en titres de l’entreprise &nbsp;?`,
       },
       answers: [
         {
-          content: 'A -  La durée courte des titres détenus',
+          content: 'A -  D’actions de l’entreprise',
+          correctAnswer: false,
+        },
+        {
+          content: `B - D’obligations de l’entreprise`,
+          correctAnswer: false,
+        },
+        {
+          content: 'C - Les deux sont possibles',
           correctAnswer: true,
-        },
-        {
-          content: `B - L’actif le plus risqué détenu`,
-          correctAnswer: false,
-        },
-        {
-          content: 'C - La proportion de titres non-monétaires détenus',
-          correctAnswer: false,
         },
       ],
       explication: {
-        __html: `L’AMF définit les fonds monétaires en fonction  de la durée des titres qui les composent. Elle distingue les fonds monétaires court terme des fonds monétaires dont les titres peuvent avoir une durée un peu plus longue et qui peuvent donc être plus rémunérateurs.`,
+        __html: `Un fonds en titres de l’entreprise est composé d’actions ou d’obligations émises par l’entreprise qui les propose ainsi aux salariés. En pourcentage du fonds, ces titres représentent au moins un tiers (fonds d’actionnariat salarié notamment) ou au plus un tiers (fonds diversifié en titres de l’entreprise) ; le solde est indifféremment constitué d’actions, d’obligations ou de titres monétaires d’autres émetteurs.`,
       },
     },
     {
       questionNumber: 6,
       question: {
-        __html: `6 - Quelle proportion d'actions Sodexo doit détenir le fonds Groupe Sodexo PEPS pour être classifié "fonds en titres de l'entreprise"&nbsp;?`,
+        __html: `6 - Pourquoi un fonds en titres de l’entreprise est-il considéré comme risqué&nbsp;?`,
       },
       answers: [
         {
-          content: 'A - 100%',
-          correctAnswer: false,
-        },
-        {
-          content: `B - ‎80%`,
-          correctAnswer: false,
-        },
-        {
-          content: 'C - Au moins 1/3',
+          content: 'A - Il est moins diversifié ',
           correctAnswer: true,
+        },
+        {
+          content: `B - Il est moins performant `,
+          correctAnswer: false,
+        },
+        {
+          content: 'C - L’horizon de placement est plus lointain',
+          correctAnswer: false,
         },
       ],
       explication: {
-        __html: `Pour être classifié « fonds en titres de l’entreprise » par l’AMF, il faut être composé d’au moins un tiers en titres de l’entreprise (fonds d’actionnariat salarié) ou encore d’au plus un tiers en titres de l’entreprise (fonds diversifié en titres de l’entreprise), le solde pouvant être indifféremment constitué d’actions, d’obligations ou de monétaire.`,
+        __html: `Il est moins diversifié car le poids des titres de l’entreprise est plus important que celui de chacun des autres titres. Sa performance va donc beaucoup dépendre de l’évolution d’un seul titre, celui de l’entreprise concernée ; cela ne signifie pas qu’il sera moins performant (l’évolution peut se faire à la hausse comme à la baisse), ni qu’il faille garder les parts du fonds plus longtemps (le fonds en titres de l’entreprise est éligible au Plan d’Épargne de l’entreprise dont la durée est de 5 ans).`,
       },
     },
     {
@@ -159,26 +159,26 @@ const module_12_blocQuiz = {
       },
       answers: [
         {
-          content: 'A - Au moins 60%',
+          content: 'A - La volatilité de sa performance ',
           correctAnswer: true,
         },
         {
-          content: `B - Au plus 60%`,
+          content: `B - Sa performance négative `,
           correctAnswer: false,
         },
         {
-          content: 'C - Au moins 80%',
+          content: 'C - Les deux',
           correctAnswer: false,
         },
       ],
       explication: {
-        __html: `Pour être classifié « fonds actions » par l’AMF, il faut être composé d’au moins 60% d’actions, le solde pouvant être indifféremment constitué d’obligations ou de monétaire.</br>Cette classification est rendue optionnelle à compter du 1er janvier 2018.`,
+        __html: `La volatilité de la performance d’un fonds fait prendre à l’épargnant le risque d’avoir à retirer son argent à un moment où son placement est perdant. C’est pour cette raison qu’il est recommandé de placer son épargne dans un fonds risqué quand on n’a pas besoin de cet argent à brève échéance. L’épargnant a ainsi le temps de voir la performance du fonds s’améliorer avant de vendre.`,
       },
     },
     {
       questionNumber: 8,
       question: {
-        __html: `8 - Que détient l’épargnant qui investit dans un FCPE&nbsp;?`,
+        __html: `8 - Que détient l’épargnant qui place de l’argent dans un Fonds Commun de Placement d’Entreprise (FCPE)&nbsp;?`,
       },
       answers: [
         {
@@ -195,13 +195,13 @@ const module_12_blocQuiz = {
         },
       ],
       explication: {
-        __html: `Le FCPE n’étant pas une société, l’épargnant qui investit dans un FCPE ne détient pas des actions du fonds mais des parts qui correspondent aux montants investis. Il n’est pas actionnaire du fonds et n’intervient pas dans la vie du fonds par un vote. Cela n’empêche en rien que le FCPE puisse lui-même être investi en actions et vote dans les Assemblées Générales des entreprises dont il détient les titres.`,
+        __html: `Le FCPE n’étant pas une société, l’épargnant qui place de l’argent dans un FCPE ne détient pas des actions du fonds mais des parts qui correspondent aux montants placés. Il n’est pas actionnaire du fonds et n’intervient pas dans la vie du fonds par un vote. Cela n’empêche en rien que le FCPE puisse lui-même être investi en actions et vote dans les Assemblées Générales des entreprises dont il détient les titres.`,
       },
     },
     {
       questionNumber: 9,
       question: {
-        __html: `9 - Comment se nomme un FCPE ouvert à des entreprises appartenant à des groupes différents&nbsp;?`,
+        __html: `9 - Comment se nomme un FCPE proposé par des entreprises appartenant à des groupes différents&nbsp;?`,
       },
       answers: [
         {
@@ -218,13 +218,14 @@ const module_12_blocQuiz = {
         },
       ],
       explication: {
-        __html: `Il s’agit d’un FCPE multi-entreprises. </br>A contrario le FCPE dédié est réservé aux salariés d’entreprises qui appartiennent à un même groupe. Il implique généralement des montants collectés suffisamment importants  pour  justifier des coûts et une gouvernance spécifiques. Si le qualificatif d’inter-entreprises n’est pas utilisé pour les FCPE, il l’est en revanche pour les plans d’épargne entreprise auxquels plusieurs entreprises de groupes différents peuvent adhérer.`,
+        __html: `Il s’agit d’un FCPE multi-entreprises. 
+		A contrario le FCPE dédié est réservé aux salariés d’entreprises qui appartiennent à un même groupe. Il implique généralement des montants collectés 		suffisamment importants pour justifier des coûts et une gouvernance spécifiques. Si le qualificatif d’inter-entreprises n’est pas utilisé pour les 		FCPE, il l’est en revanche pour les dispositifs -ou plans- d’épargne salariale auxquels plusieurs entreprises de groupes différents peuvent adhérer.`,
       },
     },
     {
       questionNumber: 10,
       question: {
-        __html: `10 - Quelle est la spécificité du fonds solidaire selon l’AMF&nbsp;?`,
+        __html: `10 -  Quelle est la spécificité du fonds solidaire&nbsp;?`,
       },
       answers: [
         {
@@ -241,7 +242,7 @@ const module_12_blocQuiz = {
         },
       ],
       explication: {
-        __html: `Selon l’AMF, le fonds solidaire est investi à hauteur de 5 à 10% en titres d’entreprises de l’économie sociale et solidaire qui, depuis la loi n°2014-856 du 31 juillet 2014, se composent des coopératives, mutuelles, fondations, associations, ou des sociétés commerciales remplissant plusieurs critères (gouvernance démocratique inscrite dans les statuts, but autre que le seul partage des résultats, bénéfices réinvestis à hauteur d’au moins 50% dans l’activité et d’au moins 20% en réserve statutaire, mission d’utilité sociale). Il ne faut pas le confondre avec le fonds ISR qui investit dans des entreprises aux pratiques responsables.`,
+        __html: `Le fonds solidaire investit, entre 5 et 10% des sommes collectées, dans les entreprises de l’économie sociale et solidaire (coopératives, mutuelles, fondations, associations, ou sociétés commerciales ayant une mission d’utilité sociale, un but autre que lucratif et une gouvernance démocratique). Il ne faut pas le confondre avec le fonds ISR qui investit dans des entreprises aux pratiques responsables.`,
       },
     },
   ],
