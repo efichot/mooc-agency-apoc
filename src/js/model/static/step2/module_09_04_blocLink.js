3ec858f4321cdd4120baabc80aea4f7a1a0aa551
sysadmintma <sysadmin@themoocagency.com>
2018-11-09 11:33:42 +0100

    5422 Modifier la synthèse du module 2 - Reporting

--- src/js/model/static/step2/module_09_04_blocLink.js
+++ src/js/model/static/step2/module_09_04_blocLink.js
@@ -7,7 +7,16 @@ const module_09_04_blocLink = {
   noChapter: true,
   name: 'Synthèse',
   firstDescription: {
-    __html: `Pour retrouver les extraits analysés, vous pouvez télécharger les fiches complètes des fonds.`,
+    __html: `5 points à retenir sur les performances :
+ 
+-       il y a plusieurs façons de mesurer la performance d’un fonds : absolue (sans référence à un indice ou un marché financier), relative (en référence à un indice ou un marché financier) ;</br>
+-       une performance relative positive indique que le fonds est en surperformance, négative qu’il est en sous-performance ;</br>
+-       la performance calendaire est calculée sur une année civile ; la performance glissante sur une durée ;</br>
+-       dans les fiches de reporting, les performances sont généralement nettes des frais prélevés par la société de gestion ;</br>
+-       placer son argent sur un fonds monétaire ou obligataire ne garantit pas de conserver l’intégralité du montant versé ; un tel placement réduit le risque de perte mais aussi l’espérance de gain.</br>
+
+Pour retrouver les extraits analysés, vous pouvez télécharger les fiches complètes des fonds.
+`,
   },
   link: '#',
 };
